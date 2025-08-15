549f12f9ba527f2a6f9d1cec8494359069a16186
AleMN <alejandromann19@gmail.com>
2025-08-15 19:37:32 -0300

    Se crea esqueleto de paginas y Navbar

--- src/pages/App/index.jsx
+++ src/pages/App/index.jsx
@@ -1,37 +1,36 @@
-import { useRoutes } from "react-router-dom";
+import { useRoutes, BrowserRouter } from "react-router-dom";
 
-import "./App.css";
 import Home from "../Home";
 import MyAccount from "../MyAccount";
 import MyOrder from "../MyOrder";
 import MyOrders from "../MyOrders";
 import SignIn from "../SignIn";
 import NotFound from "../NotFound";
+import NavBar from "../../components/NavBar";
+import "./App.css";
 
 const AppRoutes = () => {
   let routes = useRoutes([
-    {
-      path: "/",
-      element: <Home />,
-    },
+    { path: "/", element: <Home /> },
+    { path: "/mi-cuenta", element: <MyAccount /> },
+    { path: "/mi-orden", element: <MyOrder /> },
+    { path: "/mis-ordenes", element: <MyOrders /> },
+    { path: "/sign-in", element: <SignIn /> },
+    { path: "/*", element: <NotFound /> },
   ]);
 
   return routes;
 };
 
-function App() {
+const App = () => {
   return (
     <>
-      <div className="bg-red-100">
-        <Home />
-        <MyAccount />
-        <MyOrder />
-        <MyOrders />
-        <SignIn />
-        <NotFound />
-      </div>
+      <BrowserRouter>
+        <AppRoutes />
+        <NavBar />
+      </BrowserRouter>
     </>
   );
-}
+};
 
 export default App;
